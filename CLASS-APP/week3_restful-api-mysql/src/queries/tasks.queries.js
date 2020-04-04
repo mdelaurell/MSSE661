7389211a97d5534a8ea505c2b7fe73a3c335f68e
Michael De Laurell <mdelaurel@gmail.com>
2020-04-04 14:39:14 -0600

    Update to the week3_restful assignement
    Update to the week3 restful assignment to update the table created as part of the initial startup.

--- CLASS-APP/week3_restful-api-mysql/src/queries/tasks.queries.js
+++ CLASS-APP/week3_restful-api-mysql/src/queries/tasks.queries.js
@@ -1,11 +1,20 @@
-exports.CREATE_TASKS_TABLE = 'CREATE TABLE IF NOT EXISTS tasks(id int NOT NULL AUTO_INCREMENT,name varchar(255) NOT NULL,created_date DATETIME DEFAULT CURRENT_TIMESTAMP(),status varchar(10) DEFAULT \'pending\',PRIMARY KEY (id))';
+exports.CREATE_TASKS_TABLE = 'CREATE TABLE IF NOT EXISTS \
+traveler(id int NOT NULL AUTO_INCREMENT, \
+    username VARCHAR(20), \
+    password VARCHAR(32), \
+    firstName VARCHAR(25), \
+    lastName VARCHAR(30), \
+    emailAddress VARCHAR(50), \
+    created_date DATETIME DEFAULT CURRENT_TIMESTAMP(), \
+    status varchar(10) DEFAULT \'pending\', \
+    PRIMARY KEY (id))';
 
-exports.ALL_TASKS = 'SELECT * FROM tasks';
+exports.ALL_TASKS = 'SELECT * FROM traveler';
 
-exports.SINGLE_TASKS = 'SELECT * FROM tasks where id = ?';
+exports.SINGLE_TASKS = 'SELECT * FROM traveler where id = ?';
 
-exports.CREATE_TASKS = 'INSERT INTO tasks (name) VALUES (?)';
+exports.CREATE_TASKS = 'INSERT INTO traveler (name) VALUES (?)';
 
-exports.UPDATE_TASKS = 'UPDATE tasks set name = ?, status = ? WHERE id = ?';
+exports.UPDATE_TASKS = 'UPDATE traveler set name = ?, status = ? WHERE id = ?';
 
-exports.DELETE_TASKS = 'DELETE FROM tasks where id = ?';
+exports.DELETE_TASKS = 'DELETE FROM traveler where id = ?';
