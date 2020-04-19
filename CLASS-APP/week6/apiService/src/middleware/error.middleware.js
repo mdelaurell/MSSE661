ca34b9338bf849c6f62a0dce271f636fba595f36
Michael De Laurell <mdelaurel@gmail.com>
2020-04-19 14:17:14 -0600

    Updates to the class app for week 5 and starting week6
    Updates to the class app for week 5 and starting week6

---
+++ CLASS-APP/week6/apiService/src/middleware/error.middleware.js
@@ -0,0 +1,14 @@
+//Error middleware functions
+
+exports.error404 = (req, res,next) => {
+    next({message: 'Not Found', status: 404 });
+};
+
+exports.error500 = (error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+};
