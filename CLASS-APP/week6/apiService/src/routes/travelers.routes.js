8371a642d409611e842549bf54e98029d935f1a4
Michael De Laurell <mdelaurel@gmail.com>
2020-04-19 16:58:34 -0600

    Week6
    Week6 updates

--- CLASS-APP/week6/apiService/src/routes/travelers.routes.js
+++ CLASS-APP/week6/apiService/src/routes/travelers.routes.js
@@ -1,5 +1,14 @@
-const controllers = require('../controllers/travelers.controller');
 const express = require('express');
+
+const {
+    getAllTravelers,
+    createTraveler,
+    getTraveler,
+    updateTraveler,
+    deleteTraveler,
+} = require('../controllers/travelers.controller');
+
+const canAccess = require('../middleware/auth.middleware');
 
 const travelersRoutes = express.Router();
 
@@ -9,14 +18,14 @@ const travelersRoutes = express.Router();
 //  Routes for all Travelers
 //***************************************
 
-travelersRoutes.get('/', controllers.getAllTravelers).post('/', controllers.createTravelers);
+travelersRoutes.get('/', canAcess, getAllTravelers).post('/', canAccess, createTraveler);
 
 // Routes for a task by id Evaluates to '/travelers/:travelersID/
 
 travelersRoutes
-    .get('/:taskId', controllers.getAllTravelers)
-    .put('/:taskId', controllers.updateTravelers)
-    .delete('/:taskId', controllers.deleteTravelers);
+    .get('/:taskId', canAccess, getTravelers)
+    .put('/:taskId', canAccess, updateTravelers)
+    .delete('/:taskId', canAccess, deleteTravelers);
 
 module.exports = travelersRoutes;
 
