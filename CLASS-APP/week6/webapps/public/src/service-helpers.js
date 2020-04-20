4f9808bd2531462e9fcd36acdef92e154dffc6ab
Michael De Laurell <mdelaurel@gmail.com>
2020-04-19 22:55:47 -0600

    Update service-helpers.js

--- CLASS-APP/week6/webapps/public/src/service-helpers.js
+++ CLASS-APP/week6/webapps/public/src/service-helpers.js
@@ -1,25 +1,75 @@
-function _get(url) {
-    return fetch(url, {
-        method: 'GET'
-    });
-}
+const access_token = storageHasData() ? getStorage('access_token') : '';
+const token = `Bearer ${access_token}`;
 
-function _post(url, data) {
-    return fetch(url, {
+const DEFAULT_OPTIONS = {
+    headers: {
+        'Content-Type': 'application/json',
+    },
+};
+
+const DEFAULT_OPTIONS_WITH_AUTH = {
+    headers: {
+        Authorization: token,
+        'Content-Type': 'application/json',
+    },
+};
+
+const OPTIONS_WITH_AUTH = {
+    headers: {
+        Authorization: token,
+    },
+};
+
+/**
+ * 
+ * @param {string} url 
+ * @param {any} options 
+ */
+
+const _get = async(url, options = DEFAULT_OPTIONS_WITH_AUTH) => {
+    const res = await fetch(url, {
+        method: 'GET',
+        ... options,
+    });
+    return res.json();
+};
+
+/**
+ * 
+ * @param {string} url 
+ * @param {any} data 
+ * @param {any} options 
+ */
+const _post = async(url, data, options = DEFAULT_OPTIONS) => {
+    const res = await fetch(url, {
         method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        ... options,
     });
-}
-
-function _put(url, data) {
-    return fetch (url, {
+    return res.json();
+};
+/**
+ * 
+ * @param {string} url 
+ * @param {any} data 
+ * @param {any} options 
+ */
+const _put = async(url,data, options = DEFAULT_OPTIONS_WITH_AUTH) => {
+    const res = await fetch(url, {
         method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        ... options,
     });
-}
+    return res.json();
+};
+
+/**
+ * 
+ * @param {string} url 
+ * @param {any} options 
+ */
+const _delete = async (url, options = DEFAULT_OPTIONS_WITH_AUTH) => {
+    const res = await fetch(url, {
+        method: 'DELETE',
+        ... options,
+    });
+    return res.json();
+};
